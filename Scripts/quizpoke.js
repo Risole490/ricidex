c80eccabb18c0f636a33dc04adf79c9abd063782
Leoyan <71907948+Risole490@users.noreply.github.com>
2024-06-25 11:57:36 -0300

    Quiz operante. Ajustando estilização e outras funções

--- Scripts/quizpoke.js
+++ Scripts/quizpoke.js
@@ -1,9 +1,14 @@
 const meioQuiz = document.getElementById('meio-quiz');
 const btnIniciarQuiz = document.getElementById('btn-iniciar-quiz');
+const quizMensagem = document.getElementById('quiz-resposta-msg');
 
 let temporestante = 10;
 let HP = 100;
 let intervalo;
+let respostaCorreta = '';
+let streak = 0;
+let streakMax = 0;
+let rodadaShiny = false;
 
 import { getPokeInfos } from './PokeAPI/pokes.js';
 
@@ -13,7 +18,9 @@ async function atualizaTempo() {
         document.getElementById('quiz-header-tempo').textContent = temporestante;
     } else {
         let pokemon = await buscaPokemon();
-        temporestante = 10; // Reinicia o tempo
+        respostaCorreta = pokemon.pokeName;
+        respostaCorreta = respostaCorreta.toLowerCase();
+        temporestante = 11; // Reinicia o tempo
         HP -= 10; // Diminui a vida
 
         meioQuiz.innerHTML = `
@@ -54,7 +61,8 @@ async function buscaPokemon(){
     let pokeName = pokeInfos.pokeName;
     let pokeSprite = '';
     if(chanceShiny()) {
-        pokeSprite = pokeInfos.pokeSpriteShiny;    
+        pokeSprite = pokeInfos.pokeSpriteShiny;
+        rodadaShiny = true;    
     } else {
         pokeSprite = pokeInfos.pokeSpriteNormal;
     }
@@ -64,7 +72,9 @@ async function buscaPokemon(){
 
 async function iniciaQuiz(){
     try{
-        let pokemon = await buscaPokemon();   
+        let pokemon = await buscaPokemon();
+        respostaCorreta = formataResposta(pokemon.pokeName);
+        respostaCorreta = respostaCorreta.toLowerCase();
 
         temporestante = 10;
         HP = 100;
@@ -73,13 +83,45 @@ async function iniciaQuiz(){
         habilitaElementos();
         
         meioQuiz.innerHTML = `
-        <div class="middle-quiz-pokemon">
+        <div class="middle-quiz-pokemon" id="middle-quiz-pokemon">
             <h1 class="quiz-pokemon-nome" id="quiz-pokemon-nome">?</h1>
             <img src="${pokemon.pokeSprite}" alt="Pokémon do quiz" class="quiz-pokemon-img" id="quiz-pokemon-img">
         </div>
         `;
 
-        intervalo = setInterval(atualizaTempo, 1000);
+        intervalo = setInterval(atualizaTempo, 1000); // A cada 1 segundo atualiza o tempo do quiz
+    } catch(err) {
+        console.error(err);
+    }
+}
+
+function formataResposta(nome){
+    if(nome.includes('-')){
+        let nomeFormatado = nome.split('-'); // Exemplo: 'mr-mime' -> ['mr', 'mime']
+        nomeFormatado = nomeFormatado.join(' '); // ['mr', 'mime'] -> 'mr mime'
+        return nomeFormatado; // 'mr mime'
+    } else if (nome.includes('-mega')){ // Exemplo: 'charizard-mega-x' -> 'charizard mega x'
+        let nomeFormatado = nome.split('-'); // ['charizard', 'mega', 'x']
+        nomeFormatado = nomeFormatado.join(' '); // ['charizard', 'mega', 'x'] -> 'charizard mega x'
+        return nomeFormatado; // 'charizard mega x'
+    } 
+    return nome;
+}
+
+
+async function continuaQuiz(){
+    quizMensagem.textContent = '';
+
+    try {
+        let pokemon = await buscaPokemon();
+        respostaCorreta = formataResposta(pokemon.pokeName);
+        respostaCorreta = respostaCorreta.toLowerCase();
+        
+        temporestante = 10;
+
+        document.getElementById('quiz-header-tempo').textContent = temporestante;
+        document.getElementById('quiz-pokemon-nome').textContent = '?';
+        document.getElementById('quiz-pokemon-img').src = pokemon.pokeSprite;
     } catch(err) {
         console.error(err);
     }
@@ -87,8 +129,57 @@ async function iniciaQuiz(){
 
 function finalizaQuiz(){
     clearInterval(intervalo);
-    meioQuiz.innerHTML = '';
+    meioQuiz.innerHTML = `
+    <div class="middle-quiz-finalizado">
+        <h1 class="quiz-finalizado-msg">Fim de jogo!</h1>
+        <h2 class="quiz-finalizado-streak">Melhor sequência: ${streakMax}</h2>
+    `;
     alert('Fim de jogo! Tempo esgotado');
 }
 
+function verificaResposta(){
+    btnResponder.disabled = false;
+
+    let respostaUsuario = document.getElementById('quiz-input').value;
+    respostaUsuario = respostaUsuario.toLowerCase();
+    console.log(respostaUsuario);
+    console.log(respostaCorreta);
+    
+    if(respostaUsuario === '') {
+        return;
+    } else if(respostaUsuario === respostaCorreta || respostaUsuario && respostaCorreta.includes(respostaUsuario)){
+        document.getElementById('quiz-pokemon-nome').textContent = respostaCorreta.charAt(0).toUpperCase() + respostaCorreta.slice(1);
+        document.getElementById('quiz-input').value = '';
+        quizMensagem.textContent = 'Correto!';
+        streak++;
+        setTimeout(continuaQuiz, 1000);
+    } else {
+        if(HP > 0) {
+            document.getElementById('quiz-input').value = '';
+            HP -= 10;
+            document.getElementById('quiz-bottom-life').textContent = HP;
+
+            if(streak > streakMax) {
+                streakMax = streak;
+            }
+
+
+            streak = 0;
+        } else {
+            finalizaQuiz();
+            alert('Fim de jogo! O HP acabou');
+        }
+    }
+}
+
 btnIniciarQuiz.addEventListener('click', iniciaQuiz);
+
+const btnResponder = document.getElementById('btn-responder-quiz');
+
+btnResponder.addEventListener('click', verificaResposta);
+document.getElementById('quiz-input').addEventListener('keypress', function(e){
+    if(e.key === 'Enter'){
+        e.preventDefault();
+        verificaResposta();
+    }
+});
