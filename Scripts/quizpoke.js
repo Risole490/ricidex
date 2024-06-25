9e0a56415c7dd0400e327e72d907a8c0970792db
Macris <71907948+Risole490@users.noreply.github.com>
2024-06-25 16:47:38 -0300

    Quiz operante

--- Scripts/quizpoke.js
+++ Scripts/quizpoke.js
@@ -8,27 +8,25 @@ let intervalo;
 let respostaCorreta = '';
 let streak = 0;
 let streakMax = 0;
-let rodadaShiny = false;
+let tentativas = 0;
 
 import { getPokeInfos } from './PokeAPI/pokes.js';
+
+
 
 async function atualizaTempo() {
     if(temporestante > 0) {
         temporestante--;
-        document.getElementById('quiz-header-tempo').textContent = temporestante;
+        document.getElementById('quiz-header-tempo-text-num').textContent = temporestante;
     } else {
         let pokemon = await buscaPokemon();
         respostaCorreta = pokemon.pokeName;
         respostaCorreta = respostaCorreta.toLowerCase();
         temporestante = 11; // Reinicia o tempo
-        HP -= 10; // Diminui a vida
+        HP -= 5; // Diminui a vida
 
-        meioQuiz.innerHTML = `
-        <div class="middle-quiz-pokemon">
-            <h1 class="quiz-pokemon-nome" id="quiz-pokemon-nome">?</h1>
-            <img src="${pokemon.pokeSprite}" alt="Pokémon do quiz" class="quiz-pokemon-img" id="quiz-pokemon-img">
-        </div>
-        `;
+        document.getElementById('quiz-pokemon-nome').textContent = '?';
+        document.getElementById('quiz-pokemon-img').src = pokemon.pokeSprite;
 
         document.getElementById('quiz-bottom-life').textContent = HP;
         if(HP <= 0) {
@@ -79,8 +77,7 @@ async function iniciaQuiz(){
         temporestante = 10;
         HP = 100;
         document.getElementById('quiz-bottom-life').textContent = HP;
-        document.getElementById('quiz-header-tempo').textContent = temporestante;
-        habilitaElementos();
+        document.getElementById('quiz-header-tempo-text-num').textContent = temporestante;
         
         meioQuiz.innerHTML = `
         <div class="middle-quiz-pokemon" id="middle-quiz-pokemon">
@@ -111,6 +108,7 @@ function formataResposta(nome){
 
 async function continuaQuiz(){
     quizMensagem.textContent = '';
+    document.getElementById('quiz-input').value = '';
 
     try {
         let pokemon = await buscaPokemon();
@@ -119,7 +117,7 @@ async function continuaQuiz(){
         
         temporestante = 10;
 
-        document.getElementById('quiz-header-tempo').textContent = temporestante;
+        document.getElementById('quiz-header-tempo-text-num').textContent = temporestante;
         document.getElementById('quiz-pokemon-nome').textContent = '?';
         document.getElementById('quiz-pokemon-img').src = pokemon.pokeSprite;
     } catch(err) {
@@ -130,11 +128,45 @@ async function continuaQuiz(){
 function finalizaQuiz(){
     clearInterval(intervalo);
     meioQuiz.innerHTML = `
-    <div class="middle-quiz-finalizado">
+    <div class="middle-quiz-finalizado" id="middle-quiz-finalizado">
         <h1 class="quiz-finalizado-msg">Fim de jogo!</h1>
         <h2 class="quiz-finalizado-streak">Melhor sequência: ${streakMax}</h2>
+        <button class="btn-reiniciar-quiz" id="btn-reinicia-quiz">Jogar novamente</button>
+    </div>
     `;
-    alert('Fim de jogo! Tempo esgotado');
+
+    document.getElementById('quiz-header-tempo').classList.add('inativo');
+    document.getElementById('btn-finalizar-quiz').classList.add('inativo');
+    document.getElementById('bottom-quiz-iniciado').classList.add('inativo');
+    document.getElementById('quiz-input').classList.add('inativo');
+
+    document.getElementById('btn-reinicia-quiz').addEventListener('click', (e) => {
+        e.preventDefault();
+        habilitaElementosReinicio();
+        iniciaQuiz();
+    })
+};
+
+function habilitaElementosReinicio(){
+    document.getElementById('quiz-header-tempo').classList.remove('inativo');
+    document.getElementById('btn-finalizar-quiz').classList.remove('inativo');
+    document.getElementById('bottom-quiz-iniciado').classList.remove('inativo');
+    document.getElementById('quiz-input').classList.remove('inativo');
+    document.getElementById('middle-quiz-finalizado').classList.add('inativo');
+}
+
+function pulaQuiz(){
+    if(HP > 10){
+        HP -= 10;
+    } else if(HP > 0 && HP <= 10){
+        HP = 0;
+    } else {
+        finalizaQuiz();
+    }
+
+    continuaQuiz();
+    document.getElementById('quiz-input').value = '';
+    document.getElementById('quiz-bottom-life').textContent = HP;
 }
 
 function verificaResposta(){
@@ -142,28 +174,38 @@ function verificaResposta(){
 
     let respostaUsuario = document.getElementById('quiz-input').value;
     respostaUsuario = respostaUsuario.toLowerCase();
-    console.log(respostaUsuario);
-    console.log(respostaCorreta);
+    tentativas++;
     
     if(respostaUsuario === '') {
         return;
-    } else if(respostaUsuario === respostaCorreta || respostaUsuario && respostaCorreta.includes(respostaUsuario)){
+    } else if((respostaUsuario === respostaCorreta || (respostaCorreta.includes(respostaUsuario) && respostaUsuario.length === respostaCorreta.length)) && respostaUsuario){
         document.getElementById('quiz-pokemon-nome').textContent = respostaCorreta.charAt(0).toUpperCase() + respostaCorreta.slice(1);
         document.getElementById('quiz-input').value = '';
         quizMensagem.textContent = 'Correto!';
+        
+        if(HP < 100 || tentativas === 1) {
+            HP += 5;
+            if(HP > 100) HP = 100;
+            document.getElementById('quiz-bottom-life').textContent = HP;
+        } else if( HP < 100 && tentativas > 1) {
+            HP += 1;
+            if(HP > 100) HP = 100;
+            document.getElementById('quiz-bottom-life').textContent = HP;
+        }
+
+        tentativas = 0;
         streak++;
         setTimeout(continuaQuiz, 1000);
     } else {
         if(HP > 0) {
             document.getElementById('quiz-input').value = '';
-            HP -= 10;
+            quizMensagem.textContent = 'Incorreto!';
+            HP -= 1;
             document.getElementById('quiz-bottom-life').textContent = HP;
 
             if(streak > streakMax) {
                 streakMax = streak;
             }
-
-
             streak = 0;
         } else {
             finalizaQuiz();
@@ -172,14 +214,34 @@ function verificaResposta(){
     }
 }
 
-btnIniciarQuiz.addEventListener('click', iniciaQuiz);
+btnIniciarQuiz.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    habilitaElementos();
+    iniciaQuiz();
+});
+document.getElementById('btn-pular-quiz').addEventListener('click', pulaQuiz);
+document.getElementById('btn-finalizar-quiz').addEventListener('click', finalizaQuiz);
 
 const btnResponder = document.getElementById('btn-responder-quiz');
 
-btnResponder.addEventListener('click', verificaResposta);
+btnResponder.addEventListener('click', () => {
+    btnResponder.classList.add('roda');
+
+    setTimeout(function() {
+        btnResponder.classList.remove('roda');
+    }, 1000);
+    verificaResposta();
+});
+
 document.getElementById('quiz-input').addEventListener('keypress', function(e){
     if(e.key === 'Enter'){
         e.preventDefault();
+        btnResponder.classList.add('roda');
+
+        setTimeout(function() {
+            btnResponder.classList.remove('roda');
+        }, 1000);
         verificaResposta();
     }
 });
